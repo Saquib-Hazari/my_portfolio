be74a58c14de85a40ec6d504ad975c9f2d6d2c2e
Saquib Hazari <saquibhazari1000@gmail.com>
2025-02-09 19:23:53 +0530

    updated original

--- static/app.js
+++ static/app.js
@@ -25,7 +25,7 @@ document
       message: document.getElementById("message").value,
     };
 
-    let response = await fetch("http://127.0.0.1:5000/", {
+    let response = await fetch("/submit", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify(formData),
