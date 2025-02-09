e5300382af90db33e3adfba1e2df796ca9c1a91b
Saquib Hazari <saquibhazari1000@gmail.com>
2025-02-09 19:34:04 +0530

    updated original

--- app.py
+++ app.py
@@ -46,4 +46,4 @@ def submit():
     return jsonify({"message": "Data saved successfully!"}), 201
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=5000, debug=True)
